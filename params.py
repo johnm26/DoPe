262d539d9da19adebe3031e7087dcb9bf9330ba6
johnm26 <johnm26@uw.edu>
2016-02-19 15:58:31 -0800

    Filled in params.py a little

---
+++ params.py
@@ -0,0 +1,20 @@
+#####################
+# PENDULUM PARAMETERS
+#####################
+# Arm lengths and bob masses.
+l1 = 1.0
+l2 = 1.0
+m1 = 1.0
+m2 = 1.0
+
+# Initial conditions.
+t0 = 0.0
+p0 = 0.0
+td0 = 0.0
+pd0 = 0.0
+
+###############################
+# SOLUTION ALGORITHM PARAMETERS
+###############################
+# Timestep.
+dt = 0.1
